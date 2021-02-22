5db3172499f0e6f4bf64082dcf3b6265a57c1b69
Luan Oliveira <luan.augusto.xavier.oliveira@gmail.com>
2021-02-22 10:45:51 -0300

    trabalhando com os produtos

--- app.js
+++ app.js
@@ -3,19 +3,32 @@ const path = require('path');
 
 const logger = require('./middleware/logger');
 
+const produtos = require('./models/Produtos');
+
 const PORTA = process.env.PORT || 8080;
 const server = express();
 
 // Iniciar middlewares
 server.use(logger);
 
-// Lista todos os produtos
+// Home
 server.get('/', (req, res) => {
-    // res.send('Hello World');
-    // res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    return res.send('Hello World');
+    // return res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
-    var produtos = [ {id:1, nome:'Batata Doce Roxa'}, {id:2, nome:'Filé de Peito de Frango'} ];
-    res.json(produtos);
+// Lista todos os produtos
+server.get('/api/v1/produtos', (req, res) => {
+    return res.json(produtos);
+});
+
+// Buscar produto específico
+server.get('/api/v1/produtos/:id', (req, res) => {
+    let id = parseInt(req.params.id);
+    let produto = produtos.filter(produto => produto.id === id);
+
+    if ((produto.length <= 0)) return res.status(404).json({ 'message':`Nenhum produto com o id ${id}` });
+    return res.json(produto[0]);
 });
 
 // Definir pasta de arquivos estáticos
---
+++ models/Produtos.js
@@ -0,0 +1,12 @@
+const produtos = [
+    {
+        id:1,
+        nome:'Batata Doce Roxa'
+    },
+    {
+        id:2,
+        nome:'Filé de Peito de Frango'
+    }
+];
+
+module.exports = produtos;
