fc52607f18e61f6c7495bf3dd687f5597166db96
Luan Oliveira <luan.augusto.xavier.oliveira@gmail.com>
2021-02-22 10:01:18 -0300

    pasta de arquivos estátios

--- app.js
+++ app.js
@@ -6,8 +6,14 @@ const server = express();
 
 server.get('/', (req, res) => {
     // res.send('Hello World');
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    // res.sendFile(path.join(__dirname, 'public', 'index.html'));
+
+    var produtos = [ {id:1, nome:'Batata Doce Roxa'}, {id:2, nome:'Filé de Peito de Frango'} ];
+    res.json(produtos);
 });
+
+// Definir pasta de arquivos estáticos
+server.use(express.static(path.join(__dirname, 'public')));
 
 // Ouvir conexões
 server.listen(PORTA, () => console.log(`Server iniciado na porta ${PORTA}`));
